bfa2bc3b609e5c46396e97302bf84dedeb03e20b
Andrew Black <andrew@f66.dev>
2022-08-14 11:48:49 -0400

    Initial commit

---
+++ components/PublicPastes.js
@@ -0,0 +1,18 @@
+import { Table } from '@mantine/core'
+
+export default function PublicPastes() {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Publish Date</th>
+          <th>Author</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </Table>
+  )
+}
